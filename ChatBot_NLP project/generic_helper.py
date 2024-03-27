09ab622c49b8a860080fa4e4000efb97d3eedae0
Jeny Thevarajan <147185616+JenyThevarajan@users.noreply.github.com>
2024-03-27 19:04:33 +0530

    Add files via upload
    To see the demo:
    https://bot.dialogflow.com/f166f8e7-1ffa-43c4-823b-bac0225b2a12

---
+++ ChatBot_NLP project/generic_helper.py
@@ -0,0 +1,19 @@
+import re
+
+
+
+def get_str_from_food_dict(food_dict: dict):
+    return ", ".join([f"{int(value)} {key}" for key, value in food_dict.items()])
+
+if __name__ =="__main__":
+    print(get_str_from_food_dict({"samosa":2, "chole":5}))
+    #print(extract_session_id("projects/jens-chatbot-gilg/agent/sessions/5328d80c-3051-b835-cbac-adb63c5e39f6/contexts/ongoing-order"))
+
+
+def extract_session_id(session_str: str):
+    match = re.search(r"/sessions/(.*?)/contexts/", session_str)
+    if match:
+        extracted_string = match.group(1)
+        return extracted_string
+
+    return ""
